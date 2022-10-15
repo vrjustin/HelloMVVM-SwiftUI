5ae48b81d259217b815c954049a554183d8b15af
Justin Maronde <myemailisjustin@me.com>
2022-10-15 07:23:54 -0400

    Initial Commit

---
+++ HelloMVVM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloMVVM
+//
+//  Created by Justin Maronde on 10/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Justin Maronde on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
